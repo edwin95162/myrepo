1a36d9725da479078119493d709d47201e8ab901
edwingarbol95@hotmail.com <edwingarbol95@hotmail.com>
2019-03-15 12:03:38 -0500

    scripts php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,16 @@
+<center>Programación Avanzada</center
+<?php
+
+	$nombre= "Peter";
+	$apellido= "Parker";
+	$edad= 30;
+	$salario= 800;
+		
+	echo "<center>BIENVENIDOS</center><br>";  //imprimir
+
+	echo "<br>Nombre: ". $nombre ."<br>";
+	echo "Apellido: ". $apellido;
+	echo "<br>Edad: ". $edad;
+	echo "<br>Salario: ". $salario;
+?>
+<center>Fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,21 @@
+<center>Calculadora Básica</center>
+
+<?php
+
+	$num1 = 10;
+	$num2 = 5;
+
+	/*	
+	$suma = $num1 + $num2;
+	$resta = $num1 - $num2;
+	$mult = $num1 * $num2;
+	$div = $num1 / $num2;	*/
+	
+	echo "<center>";
+	echo "<br>La suma es: ". ($num1 + $num2);
+	echo "<br>La resta es: ". ($num1 - $num2);
+	echo "<br>La multiplación es: ". ($num1 * $num2);
+	echo "<br>La división es: ". ($num1 / $num2);
+	echo "</center>";
+	
+?>
